9f65dd21ad905be0014d273bbe4d6c64ad6e53ab
Brennon Nuckols <brennuck@gmail.com>
2019-03-18 15:53:47 -0600

    Doing Styling

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -6,7 +6,7 @@ const HeaderTitle = () => {
         <div className="header-title-container">
             <h2 className="header-title">Lambda School</h2>      
             <p className="header-link">@LambdaSchool</p>
-            <p className="header-time" />
+            <p className="header-time">Jan 26</p>
         </div>
     )
 }
