da60329d9dab72191cdfe93f6c6c7e59c3919e2b
Brennon Nuckols <brennuck@gmail.com>
2019-03-18 17:05:12 -0600

    MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const CardContent = () => {
     return <div className="card-content">
-        <h1>Get started with React</h1>
+        <h1 className="card-title">Get started with React</h1>
         <p>
             React makes it painless to create interactive UIs. Design simple views for each state in your application.
         </p>
