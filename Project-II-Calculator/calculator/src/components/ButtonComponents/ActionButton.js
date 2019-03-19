abf83f3b0f545a686a2ca7dab3634364e1f80b1a
Brennon Nuckols <brennuck@gmail.com>
2019-03-19 14:06:08 -0600

    super basic beginning

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = () => {
+    return <div>
+        <h1>symbol</h1>
+        <h1>symbol</h1>
+        <h1>symbol</h1>
+    </div>
+}
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = () => {
+    return <div>
+        <h1>1</h1>
+        <h1>2</h1>
+        <h1>3</h1>
+    </div>
+}
+
+export default NumberButton;
